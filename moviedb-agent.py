6e7481380ef70fe87bd160791e138dd6e94f7998
Sergij Cy`najlo <funeral20@gmail.com>
2018-10-21 16:47:36 +0300

    Update moviedb-agent.py

--- moviedb-agent.py
+++ moviedb-agent.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+
+
+from secrets import TMDB_API_KEY
+import requests
+# import json
+
+query = 'Enemy+of+the+State'
+url = 'https://api.themoviedb.org/3/search/movie?api_key=' + \
+    TMDB_API_KEY+'&query=' + query
+req = requests.get(url)
+answer = req.json()
+print(answer)
